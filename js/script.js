3e17265fb9a914a0999211e94468554a0c88d4e0
M1-89-loulwah <76619506+M1-89-loulwah@users.noreply.github.com>
2021-01-02 21:30:34 +0300

    question 1&2,done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log(`23`);
+console.log(23*5);
+console.log(`حين أبلغ  ${23*5} سيبدأ زحل باستقبال المبرمجين على سطحه`);
+let numberofyear = (2021-1969);
+console.log(numberofyear);
+console.log(`${numberofyear} هو عدد الأعوام التي مرت على صعود الانسان على سطح القمر `);
+//اجابة السؤال الأول
